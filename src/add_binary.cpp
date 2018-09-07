477976f7d7265c31e45afeb80378c85ab6f0a09b
Masahisa Konno <masahisa.konno.dev@gmail.com>
2018-09-07 09:46:37 +0900

    Add comment to add_binary

--- src/add_binary.cpp
+++ src/add_binary.cpp
@@ -1,5 +1,14 @@
 #include "add_binary.h"
 
+/**
+ * @fn void add_binary(int* in1, int* in2, int* out, int size)
+ * @brief calculate sum of the two input binary arrays
+ * @param in1 the first input array consists of binary value {0, 1}
+ * @param in2 the second input array consists of binary value {0, 1}
+ * @param out the sum of two input arrays, the size of the out is size + 1
+ * @param size the size of input arrays, in1 and in2
+ * @return void
+ */
 void add_binary(int* in1, int* in2, int* out, int size)
 {
     int carry = 0;
