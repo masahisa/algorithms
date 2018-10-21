3f2380b83eab0494fb290a0b663745ff4afdbc7c
Masahisa Konno <masahisa.konno.dev@gmail.com>
2018-10-21 15:29:03 +0900

    Fix typo

--- include/queue.h
+++ include/queue.h
@@ -20,7 +20,7 @@ int enqueue(int* a, int* head, int* tail, int size, int x);
  * @param head the head of the queue
  * @param tail the tail of the queue
  * @param size the size of array
- * @return when detected underflow, return error code INT32_MIN
+ * @return the value deleted or when detected underflow, return error code INT32_MIN
  */
 int dequeue(int* a, int* head, int* tail, int size);
 
